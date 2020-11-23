40c857d94971e690a91360fdab41cc75763532ec
Layton Williams <54451152+lwill363@users.noreply.github.com>
2020-11-22 22:06:07 -0500

    Update headers

--- Request_services.py
+++ Request_services.py
@@ -12,14 +12,14 @@ def handle_speed_request():
     speed_measure2 = request.form.get("speed_measure2")
     speed_value = request.form.get("speed_value")
 
-    speedheaders = {"Content-Type": "application/json"}
+    headers = {"Content-Type": "application/json"}
 
     if speed_measure1 == 'mph' and speed_measure2 == 'kph':
         response = make_response(
             {'kph': speed_value * 1.609344},
             200
         )
-        response.headers = speedheaders
+        response.headers = headers
         return response
     elif speed_measure1 == 'kph' and speed_measure2 == 'mph':
         response = make_response(
@@ -43,14 +43,14 @@ def handle_weight_request():
     weight_measure2 = request.form.get("weight_measure2")
     weight_value = request.form.get("weight_value")
 
-    weightheaders = {"Content-Type": "application/json"}
+    headers = {"Content-Type": "application/json"}
 
     if weight_measure1 == 'lbs' and weight_measure2 == 'kg':
         response = make_response(
             {'kg': weight_value * 0.45359237},
             200
         )
-        response.headers = weightheaders
+        response.headers = headers
         return response
 
     elif weight_measure1 == 'kg' and weight_measure2 == 'lbs':
@@ -76,14 +76,14 @@ def handle_temp_request():
     temp_measure2 = request.form.get("temp_measure2")
     temp_value = request.form.get("temp_value")
 
-    tempheaders = {"Content-Type": "application/json"}
+    headers = {"Content-Type": "application/json"}
 
     if temp_measure1 == 'fahren' and temp_measure2 == 'cels':
         response = make_response(
             {'cels': (temp_value - 32) * (5 / 9)},
             200
         )
-        response.headers = tempheaders
+        response.headers = headers
         return response
 
     elif temp_measure1 == 'cels' and temp_measure2 == 'fahren':
@@ -102,4 +102,4 @@ def handle_temp_request():
         return response
 
 
-app.run(port=3533)
+app.run(port=3534)
