95660256aea565700fe5c5f42627f2fd4a8f39a0
TheDanchou <50221478+TheDanchou@users.noreply.github.com>
2020-11-22 22:01:24 -0500

    Update Request_services.py

--- Request_services.py
+++ Request_services.py
@@ -12,14 +12,15 @@ def handle_speed_request():
     speed_measure2 = request.form.get("speed_measure2")
     speed_value = request.form.get("speed_value")
 
-    headers = {"Content-Type": "application/json"}
+    speedheaders = {"Content-Type": "application/json"}
 
     if speed_measure1 == 'mph' and speed_measure2 == 'kph':
         response = make_response(
             {'kph': speed_value * 1.609344},
             200
         )
-
+        response.headers = speedheaders
+        return response
     elif speed_measure1 == 'kph' and speed_measure2 == 'mph':
         response = make_response(
             {'mph': speed_value / 1.609344},
@@ -42,13 +43,15 @@ def handle_weight_request():
     weight_measure2 = request.form.get("weight_measure2")
     weight_value = request.form.get("weight_value")
 
-    headers = {"Content-Type": "application/json"}
+    weightheaders = {"Content-Type": "application/json"}
 
     if weight_measure1 == 'lbs' and weight_measure2 == 'kg':
         response = make_response(
             {'kg': weight_value * 0.45359237},
             200
         )
+        response.headers = weightheaders
+        return response
 
     elif weight_measure1 == 'kg' and weight_measure2 == 'lbs':
         response = make_response(
@@ -73,13 +76,15 @@ def handle_temp_request():
     temp_measure2 = request.form.get("temp_measure2")
     temp_value = request.form.get("temp_value")
 
-    headers = {"Content-Type": "application/json"}
+    tempheaders = {"Content-Type": "application/json"}
 
     if temp_measure1 == 'fahren' and temp_measure2 == 'cels':
         response = make_response(
             {'cels': (temp_value - 32) * (5 / 9)},
             200
         )
+        response.headers = tempheaders
+        return response
 
     elif temp_measure1 == 'cels' and temp_measure2 == 'fahren':
         response = make_response(
