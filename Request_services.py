b1187d3cc76486f2e29535df493bb12d11eebe7b
TheDanchou <50221478+TheDanchou@users.noreply.github.com>
2020-11-22 21:16:51 -0500

    Update Request_services.py

--- Request_services.py
+++ Request_services.py
@@ -11,19 +11,19 @@ def handle_speed_request():
     speed_measure1 = request.form.get("speed_measure1")
     speed_measure2 = request.form.get("speed_measure2")
     speed_value = request.form.get("speed_value")
-    
+
     headers = {"Content-Type": "application/json"}
+
     if speed_measure1 == 'mph' and speed_measure2 == 'kph':
         response = make_response(
-        {'kph': speed_value * 1.609344},
-        200
+            {'kph': speed_value * 1.609344},
+            200
         )
-        response.headers = headers
-        return response
+
     elif speed_measure1 == 'kph' and speed_measure2 == 'mph':
         response = make_response(
-        {'mph': speed_value / 1.609344},
-        200
+            {'mph': speed_value / 1.609344},
+            200
         )
         response.headers = headers
         return response
@@ -38,36 +38,63 @@ def handle_speed_request():
 
 @app.route('/weight_request', methods=['POST'])
 def handle_weight_request():
-    weight_measure = request.form.get("weight_measure")
+    weight_measure1 = request.form.get("weight_measure1")
+    weight_measure2 = request.form.get("weight_measure2")
     weight_value = request.form.get("weight_value")
-    values = weight_measure + weight_value
 
     headers = {"Content-Type": "application/json"}
 
-    response = make_response(
+    if weight_measure1 == 'lbs' and weight_measure2 == 'kg':
+        response = make_response(
+            {'kg': weight_value * 0.45359237},
+            200
+        )
 
-        {'values': values},
-        200
-    )
-    response.headers = headers
-    return response
+    elif weight_measure1 == 'kg' and weight_measure2 == 'lbs':
+        response = make_response(
+            {'lbs': weight_value / 0.45359237},
+            200
+        )
+        response.headers = headers
+        return response
+    else:
+        response = make_response(
+            {'error': 'Invalid weight request format'},
+            400
+        )
+        response.headers = headers
+        return response
+
 
 
 @app.route('/temp_request', methods=['POST'])
 def handle_temp_request():
-    temp_measure = request.form.get("temp_measure")
+    temp_measure1 = request.form.get("temp_measure1")
+    temp_measure2 = request.form.get("temp_measure2")
     temp_value = request.form.get("temp_value")
-    values = temp_measure + temp_value
 
     headers = {"Content-Type": "application/json"}
 
-    response = make_response(
+    if temp_measure1 == 'fahren' and temp_measure2 == 'cels':
+        response = make_response(
+            {'cels': (temp_value - 32) * (5 / 9)},
+            200
+        )
 
-        {'values': values},
-        200
-    )
-    response.headers = headers
-    return response
+    elif temp_measure1 == 'cels' and temp_measure2 == 'fahren':
+        response = make_response(
+            {'fahren': (temp_value * (9 / 5)) + 32},
+            200
+        )
+        response.headers = headers
+        return response
+    else:
+        response = make_response(
+            {'error': 'Invalid temp request format'},
+            400
+        )
+        response.headers = headers
+        return response
 
 
 app.run(port=3533)
