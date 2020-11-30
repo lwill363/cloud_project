4693ded830645f07978bab1c22c2c9520a62b3d4
TheDanchou <50221478+TheDanchou@users.noreply.github.com>
2020-11-29 23:36:29 -0500

    Update Request_services.py

--- Request_services.py
+++ Request_services.py
@@ -1,9 +1,11 @@
 from flask import Flask, make_response, request
+import pymysql
 import json
 
 app = Flask(__name__)
-
-
+# format should be pymysql.connect("localhost", "user", "password", "dbname")
+db = pymysql.connect("localhost", "user", "password", "dbname")
+cursor = db.cursor()
 # app.config["debug"] = True
 
 @app.route('/speed_request', methods=['POST'])
@@ -15,18 +17,28 @@ def handle_speed_request():
     headers = {"Content-Type": "application/json"}
 
     if speed_measure1 == 'mph' and speed_measure2 == 'kph':
+        result = speed_value * 1.609344
+        query = "INSERT into conversions(Mph, kph) values (%s, %s)"
+        cursor.execute(query, (speed_value, result))
+        db.commit()
         response = make_response(
-            {'kph': speed_value * 1.609344},
+            {'kph': result},
             200
         )
         response.headers = headers
+        db.close()
         return response
     elif speed_measure1 == 'kph' and speed_measure2 == 'mph':
+        result = speed_value / 1.609344
+        query = "INSERT into conversions(Kph, Mph) values (%s, %s)"
+        cursor.execute(query, (speed_value, result))
+        db.commit()
         response = make_response(
-            {'mph': speed_value / 1.609344},
+            {'mph': result},
             200
         )
         response.headers = headers
+        db.close()
         return response
     else:
         response = make_response(
@@ -34,6 +46,7 @@ def handle_speed_request():
             400
         )
         response.headers = headers
+        db.close()
         return response
 
 
@@ -46,19 +59,29 @@ def handle_weight_request():
     headers = {"Content-Type": "application/json"}
 
     if weight_measure1 == 'lbs' and weight_measure2 == 'kg':
+        result = weight_value * 0.45359237
+        query = "INSERT into conversions(lbs, kg) values (%s, %s)"
+        cursor.execute(query, (weight_value, result))
+        db.commit()
         response = make_response(
-            {'kg': weight_value * 0.45359237},
+            {'kg': result},
             200
         )
         response.headers = headers
+        db.close()
         return response
 
     elif weight_measure1 == 'kg' and weight_measure2 == 'lbs':
+        result = weight_value / 0.45359237
+        query = "INSERT into conversions(kg, lbs) values (%s, %s)"
+        cursor.execute(query, (weight_value, result))
+        db.commit()
         response = make_response(
-            {'lbs': weight_value / 0.45359237},
+            {'lbs': result},
             200
         )
         response.headers = headers
+        db.close()
         return response
     else:
         response = make_response(
@@ -66,6 +89,7 @@ def handle_weight_request():
             400
         )
         response.headers = headers
+        db.close()
         return response
 
 
@@ -79,19 +103,29 @@ def handle_temp_request():
     headers = {"Content-Type": "application/json"}
 
     if temp_measure1 == 'fahren' and temp_measure2 == 'cels':
+        result = (temp_value - 32) * (5 / 9)
+        query = "INSERT into conversions(Fahrenheit, Celsius) values (%s, %s)"
+        cursor.execute(query, (temp_value, result))
+        db.commit()
         response = make_response(
-            {'cels': (temp_value - 32) * (5 / 9)},
+            {'cels': result},
             200
         )
         response.headers = headers
+        db.close()
         return response
 
     elif temp_measure1 == 'cels' and temp_measure2 == 'fahren':
+        result = (temp_value * (9 / 5)) + 32
+        query = "INSERT into conversions(Celsius, Fahrenheit) values (%s, %s)"
+        cursor.execute(query, (temp_value, result))
+        db.commit()
         response = make_response(
-            {'fahren': (temp_value * (9 / 5)) + 32},
+            {'fahren': result},
             200
         )
         response.headers = headers
+        db.close()
         return response
     else:
         response = make_response(
@@ -99,6 +133,7 @@ def handle_temp_request():
             400
         )
         response.headers = headers
+        db.close()
         return response
 
 
